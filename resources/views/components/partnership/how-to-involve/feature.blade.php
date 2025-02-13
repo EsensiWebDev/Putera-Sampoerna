74b48b6f5649a274e92b7d1fdc80cd3165528a9a
crevasse02 <wiboworaffi82@gmail.com>
2025-02-13 10:33:59 +0700

    update

--- resources/views/components/partnership/how-to-involve/feature.blade.php
+++ resources/views/components/partnership/how-to-involve/feature.blade.php
@@ -3,9 +3,8 @@
         <div class="row g-0">
             <div class="col-lg-12 d-xxl-flex col-12 col-md-9">
                 <p class="fs-5 fw-light mb-xxl-0" style="font-family: Campton;">
-                    {{ __('Your support and partnership will enable us to continue our work to design a bright future for the nation') }}.<br><br>{{ __('An archipelago full of promise, Indonesia, as well as its citizens, have great potential to compete in the global stage. However, many obstacles impede the progress of the country, including minimum access to quality education.') }}<br><br>{{ __('In 2014, The Learning Curve report by Pearson published education data about 40 countries, and Indonesia occupied the last position for educational attainment. Every day, we work on empowering more Indonesians at all ages to have the chance to fulfil their full potentials and be the leaders in their chosen fields.') }}<br><br>{!!
-                    __('Let us all contribute to this worthy cause. <a href="https://psfoutreach.com/jadi-relawan/">Click
-                        here</a> and join us in uplifting the quality of education in Indonesia.') }
+                    {{ __('Your support and partnership will enable us to continue our work to design a bright future for the nation') }}.<br><br>{{ __('An archipelago full of promise, Indonesia, as well as its citizens, have great potential to compete in the global stage. However, many obstacles impede the progress of the country, including minimum access to quality education.') }}<br><br>{{ __('In 2014, The Learning Curve report by Pearson published education data about 40 countries, and Indonesia occupied the last position for educational attainment. Every day, we work on empowering more Indonesians at all ages to have the chance to fulfil their full potentials and be the leaders in their chosen fields.') }}<br><br>
+                   {!! __('Let us all contribute to this worthy cause. <a href="https://psfoutreach.com/jadi-relawan/">Click here</a> and join us in uplifting the quality of education in Indonesia.') !!}
                 </p>
             </div>
         </div>
