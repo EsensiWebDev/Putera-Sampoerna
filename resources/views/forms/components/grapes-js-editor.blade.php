9243fb7eae09c6d0facb92f255301e5f73f73f7c
crevasse02 <wiboworaffi82@gmail.com>
2025-02-20 15:06:33 +0700

    test

--- resources/views/forms/components/grapes-js-editor.blade.php
+++ resources/views/forms/components/grapes-js-editor.blade.php
@@ -191,7 +191,7 @@
                                                 <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'>
                                             </noscript>
                                             <link rel='stylesheet' href='{{ asset("assets/css/styles.min.css") }}'>
-                                            <link rel='stylesheet' href='{{ asset("assets/css/app.css") }}'>
+                                            {{-- <link rel='stylesheet' href='{{ asset("assets/css/app.css") }}'> --}}
                                             <style>
                                                 ${savedState.css}
                                             </style>
