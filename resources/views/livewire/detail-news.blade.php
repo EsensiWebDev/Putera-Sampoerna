c63ddbca4a1eecc691d85af623e44d4a5ac909ba
crevasse02 <wiboworaffi82@gmail.com>
2025-02-23 13:37:12 +0700

    bug fix

--- resources/views/livewire/detail-news.blade.php
+++ resources/views/livewire/detail-news.blade.php
@@ -2,43 +2,44 @@
     $locale = app()->getLocale(); // Get the current locale
 
     // Set title based on locale
-    $metaTitle =
-        $article->meta_title ??
-        ($locale == 'id'
-            ? $article->title_indonesia ?? ($article->title_english ?? '')
-            : $article->title_english ?? ($article->title_indonesia ?? ''));
-    // Set description based on locale
-    $metaDescription =
-        $locale == 'id'
-            ? $article->meta_description_ind ?? ($article->meta_description ?? '')
-            : $article->meta_description ?? ($article->meta_description_ind ?? '');
+    if ($article !== null) {
+        $metaTitle =
+            $article->meta_title ??
+            ($locale == 'id'
+                ? $article->title_indonesia ?? ($article->title_english ?? '')
+                : $article->title_english ?? ($article->title_indonesia ?? ''));
+        // Set description based on locale
+        $metaDescription =
+            $locale == 'id'
+                ? $article->meta_description_ind ?? ($article->meta_description ?? '')
+                : $article->meta_description ?? ($article->meta_description_ind ?? '');
 
-    // Set keywords based on locale
-    $metaKeywords =
-        $locale == 'id'
-            ? $article->keyword_ind ?? ($article->keyword ?? '')
-            : $article->keyword ?? ($article->keyword_ind ?? '');
+        // Set keywords based on locale
+        $metaKeywords =
+            $locale == 'id'
+                ? $article->keyword_ind ?? ($article->keyword ?? '')
+                : $article->keyword ?? ($article->keyword_ind ?? '');
 
-    // Set Open Graph image
-    $ogImage = asset($article->thumbnail ?? '');
-    $hrefSlugId =  $article->slug_ind;
-    $hrefSlugEn = $article->slug;
-    $publishedAt = $article->created_at;
-
+        // Set Open Graph image
+        $ogImage = asset($article->thumbnail ?? '');
+        $hrefSlugId = $article->slug_ind;
+        $hrefSlugEn = $article->slug;
+        $publishedAt = $article->created_at;
+    }
 @endphp
 
-@section('title', $metaTitle)
-@section('author', $author->name)
-@section('meta_description', $metaDescription)
-@section('meta_keywords', $metaKeywords)
-@section('href_slug_ind', $hrefSlugId)
-@section('href_slug_en', $hrefSlugEn)
-
-@section('published_at', $publishedAt)
-
-@section('og_title', $metaTitle)
-@section('og_description', $metaDescription)
-@section('og_image', $ogImage)
+@if($article !== null)
+    @section('title', $metaTitle)
+    @section('author', isset($author) ? $author->name : 'Default Author')
+    @section('meta_description', $metaDescription)
+    @section('meta_keywords', $metaKeywords)
+    @section('href_slug_ind', $hrefSlugId)
+    @section('href_slug_en', $hrefSlugEn)
+    @section('published_at', $publishedAt)
+    @section('og_title', $metaTitle)
+    @section('og_description', $metaDescription)
+    @section('og_image', $ogImage)
+@endif
 
 <section id="news" style="{{ is_null($article) ? 'background-color: #2b3035' : '' }}">
     @if (is_null($article))
@@ -48,30 +49,35 @@
     @else
         <section>
             <div class="d-flex align-items-end position-relative"
-            style="height: 600px; 
+                style="height: 600px; 
                    background: linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0) 100%), 
                                url('{{ str_contains($article->thumbnail, '/uploads') ? asset($article->thumbnail) : asset('storage/' . $article->thumbnail) }}') center / cover no-repeat;">
-           
-           <div style="position: absolute; bottom: 0; left: 0; width: 100%; height: 200px;
+
+                <div
+                    style="position: absolute; bottom: 0; left: 0; width: 100%; height: 200px;
                        background: linear-gradient(to top, rgba(0, 0, 0, 0.6), transparent);">
-           </div>
-       
-           <div class="container" style="padding-bottom: 64px; position: relative; z-index: 2;">
-               <div class="row">
-                   <div class="col-md-12">
-                       @php
-                           $locale = app()->getLocale();
-                           $title = $locale == 'id' ? ($article->title_indonesia ?? $article->title_english) 
-                                                    : ($article->title_english ?? $article->title_indonesia);
-                           $content = $locale == 'id' ? ($article->content_indonesia ?? $article->content_english) 
-                                                      : ($article->content_english ?? $article->content_indonesia);
-                       @endphp
-                       <h1 style="font-family: Campton; color: white !important;">{!! $title !!}</h1>
-                   </div>
-               </div>
-           </div>
-       </div>
-       
+                </div>
+
+                <div class="container" style="padding-bottom: 64px; position: relative; z-index: 2;">
+                    <div class="row">
+                        <div class="col-md-12">
+                            @php
+                                $locale = app()->getLocale();
+                                $title =
+                                    $locale == 'id'
+                                        ? $article->title_indonesia ?? $article->title_english
+                                        : $article->title_english ?? $article->title_indonesia;
+                                $content =
+                                    $locale == 'id'
+                                        ? $article->content_indonesia ?? $article->content_english
+                                        : $article->content_english ?? $article->content_indonesia;
+                            @endphp
+                            <h1 style="font-family: Campton; color: white !important;">{!! $title !!}</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </section>
         <section id="content" class="px-4 py-4 mx-4 image-handles">
             {!! $content !!}
@@ -139,7 +145,7 @@
     <script>
         document.addEventListener("DOMContentLoaded", function() {
             const links = document.querySelectorAll('#content a');
-            
+
             links.forEach(link => {
                 link.setAttribute('target', '_blank');
             });
