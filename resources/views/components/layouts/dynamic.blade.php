8a95294072fb577987067764f28b551eb5905a21
crevasse02 <wiboworaffi82@gmail.com>
2025-04-08 14:10:16 +0700

    update google

--- resources/views/components/layouts/dynamic.blade.php
+++ resources/views/components/layouts/dynamic.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="google-site-verification" content="CVRSko63NpmBbj8wZevCow5jfiDXDWpvaNdtOvLCum8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Putera Sampoerna Foundation {{ ' - ' . $page->title}}</title>
     <link rel="icon" type="image/webp" sizes="113x128" href="{{ asset("assets/img/Icon/Five%20Icon%20PSF.webp") }}">
