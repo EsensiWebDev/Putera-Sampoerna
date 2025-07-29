946c1e4ec2890fc99c5fe148af6072c75d82e3f5
crevasse02 <wiboworaffi82@gmail.com>
2025-07-29 11:17:43 +0700

    Update psf

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lead;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\Console\Input\Input;
@@ -9,10 +11,36 @@ use Symfony\Component\Console\Input\Input;
 class ContactController extends Controller
 {
     public function store(Request $request) {
-        $validate = $request->validate([
-            'g-recaptcha-response' => 'required|captcha'
-        ]);
+        // $validate = $request->validate([
+        //     'g-recaptcha-response' => 'required|captcha'
+        // ]);
 
+        try {
+            $validatedData = $request->validate([
+                'name'      => 'required|string|max:255',
+                'phone_number'      => 'required|numeric|digits_between:10,15',
+                'email'             => 'required|email|max:255',
+                'messages'          => 'required|string',
+            ]);
+            
+            $sanitizedData = array_map(function ($value) {
+                if (is_string($value)) {
+                    $value = strip_tags(trim($value)); // Strip HTML tags and trim whitespace
+                    $value = str_replace(["\r\n", "\n", "\r"], ' ', $value); // Replace newlines with a space
+                }
+                return $value;
+            }, $validatedData);
+
+            // Save the form data to the database
+            Lead::create($sanitizedData);
+
+
+            return redirect()->back()->with('success', 'Data successfully saved!')->withFragment('#formSection');
+        } catch (\Exception $e) {
+            // Log error for debugging (optional)
+            Log::error($e->getMessage());
+            return redirect()->back()->with('error', 'Failed to save data. Please try again.')->withFragment('#formSection');
+        }
 
         // proses save
     }
--- resources/views/pages/contact-us.blade.php
+++ resources/views/pages/contact-us.blade.php
@@ -1,10 +1,11 @@
 <x-layouts.app>
     <section>
-        <div class="d-flex align-items-end" style="height: 600px;background: url({{ asset("assets/img/Contact%20Us/contact%20us.jpg") }}) center / cover no-repeat;">
+        <div class="d-flex align-items-end"
+            style="height: 600px;background: url({{ asset('assets/img/Contact%20Us/contact%20us.jpg') }}) center / cover no-repeat;">
             <div class="container" style="padding-bottom: 64px;">
                 <div class="row">
                     <div class="col-md-12">
-                        <h1 style="font-family: Campton;color: var(--bs-light);">{{ __("Contact Us") }}</h1>
+                        <h1 style="font-family: Campton;color: var(--bs-light);">{{ __('Contact Us') }}</h1>
                     </div>
                 </div>
             </div>
@@ -13,31 +14,63 @@
     <section style="margin-top: 200px;margin-bottom: 200px;">
         <section class="position-relative py-4 py-xl-5">
             <div class="container">
+                @if (session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
+
+                {{-- Menampilkan pesan error --}}
+                @if (session('error'))
+                    <div class="alert alert-danger">
+                        {{ session('error') }}
+                    </div>
+                @endif
                 <div class="row mb-5">
                     <div class="col-md-8 col-lg-12 col-xl-6 offset-lg-1 text-center mx-auto">
-                        <h1 class="display-5 fw-normal" style="font-family: Campton;color: var(--bs-black);">{{ __("Partner with Us to Transform Education in Indonesia") }}</h1>
-                        <p class="fs-5 fw-light w-lg-50 my-5" style="color: #555770;font-family: Campton;margin-top: 0px;margin-bottom: 0px;">{{ __("Your partnership will enable us to reach more students, improve teaching quality, and create a brighter future for generations to come.") }}</p>
+                        <h1 class="display-5 fw-normal" style="font-family: Campton;color: var(--bs-black);">
+                            {{ __('Partner with Us to Transform Education in Indonesia') }}</h1>
+                        <p class="fs-5 fw-light w-lg-50 my-5"
+                            style="color: #555770;font-family: Campton;margin-top: 0px;margin-bottom: 0px;">
+                            {{ __('Your partnership will enable us to reach more students, improve teaching quality, and create a brighter future for generations to come.') }}
+                        </p>
                     </div>
                 </div>
                 <div class="row d-flex justify-content-center">
                     <div class="col-12 col-md-8 col-lg-8 col-xl-7 col-xxl-7 offset-0 offset-xxl-0">
                         <div class="card mb-5">
-                            <form action="/contact" method="POST" class="d-flex flex-column px-4 py-4" style="width: auto;">
+                            <form action="/contact" method="POST" class="d-flex flex-column px-4 py-4"
+                                style="width: auto;">
                                 @csrf
-                                <div class="py-3"><label class="form-label" style="font-family: Campton;">{{ __("Name") }}<span class="text-danger">*</span></label><input class="form-control" type="text"></div>
+                                <div class="py-3"><label class="form-label"
+                                        style="font-family: Campton;">{{ __('Name') }}<span
+                                            class="text-danger">*</span></label><input class="form-control"
+                                        name="name" type="text"></div>
                                 <div class="d-flex flex-row">
-                                    <div class="flex-fill py-3 pe-2"><label class="form-label" style="font-family: Campton;">{{ __("Phone Number") }}<span class="text-danger">*</span></label><input class="form-control" type="tel"></div>
-                                    <div class="flex-fill py-3 ps-2"><label class="form-label" style="font-family: Campton;">Email<span class="text-danger">*</span></label><input class="form-control" type="email"></div>
+                                    <div class="flex-fill py-3 pe-2"><label class="form-label"
+                                            style="font-family: Campton;">{{ __('Phone Number') }}<span
+                                                class="text-danger">*</span></label><input class="form-control"
+                                            name="phone_number" type="tel"></div>
+                                    <div class="flex-fill py-3 ps-2"><label class="form-label"
+                                            style="font-family: Campton;">Email<span
+                                                class="text-danger">*</span></label><input class="form-control"
+                                            name="email" type="email"></div>
                                 </div>
-                                <div class="py-3"><label class="form-label" style="font-family: Campton;">{{ __("Your Message") }}<span class="text-danger">*</span></label><textarea class="form-control" style="height: 184px;"></textarea></div>
-                                @if ($errors->has('g-recaptcha-response'))
+                                <div class="py-3"><label class="form-label"
+                                        style="font-family: Campton;">{{ __('Your Message') }}<span
+                                            class="text-danger">*</span></label>
+                                    <textarea class="form-control" name="messages" style="height: 184px;"></textarea>
+                                </div>
+                                {{-- @if ($errors->has('g-recaptcha-response'))
                                     <span class="help-block text-danger">
                                         <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
                                     </span>
                                 @endif
                                 {!! \Anhskohbo\NoCaptcha\Facades\NoCaptcha::renderJs() !!}
-                                {!! \Anhskohbo\NoCaptcha\Facades\NoCaptcha::display() !!}
-                                <div class="d-flex d-lg-flex justify-content-end justify-content-lg-end mt-4"><button class="btn btn-primary" type="submit" style="width: 120px;">{{ __("Submit") }}</button></div>
+                                {!! \Anhskohbo\NoCaptcha\Facades\NoCaptcha::display() !!} --}}
+                                <div class="d-flex d-lg-flex justify-content-end justify-content-lg-end mt-4"><button
+                                        class="btn btn-primary" type="submit"
+                                        style="width: 120px;">{{ __('Submit') }}</button></div>
                             </form>
                         </div>
                     </div>
