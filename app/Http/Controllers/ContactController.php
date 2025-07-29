56efbe3ba88132f0be2130c5941b8e1e0c161735
crevasse02 <wiboworaffi82@gmail.com>
2025-07-29 16:23:08 +0700

    update composer and contact form

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\Lead;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Symfony\Component\Console\Input\Input;
+use Google_Client;
+use Google_Service_Sheets;
+use Google_Service_Sheets_ValueRange;
 
 class ContactController extends Controller
 {
+    protected $spreadsheetId = '1JCLCGN4UtaNp1akvF5ZaVjaMGxp2O4OInHqHnMMrP40'; // Replace with your Google Spreadsheet ID
+
     public function store(Request $request) {
         // $validate = $request->validate([
         //     'g-recaptcha-response' => 'required|captcha'
@@ -34,7 +37,7 @@ class ContactController extends Controller
             // Save the form data to the database
             Lead::create($sanitizedData);
 
-
+            $this->sendDataToGoogleSheets($sanitizedData);
             return redirect()->back()->with('success', 'Data successfully saved!')->withFragment('#formSection');
         } catch (\Exception $e) {
             // Log error for debugging (optional)
@@ -43,5 +46,43 @@ class ContactController extends Controller
         }
 
         // proses save
+    }
+
+     protected function sendDataToGoogleSheets(array $data)
+    {
+        // Set up Google Sheets client
+        $submissionDate = now()->toDateTimeString();
+
+        $client = new Google_Client();
+        $client->setAuthConfig(storage_path('app/psf-leads-api.json')); // Path to your service account JSON key
+        $client->addScope(Google_Service_Sheets::SPREADSHEETS);
+
+        $service = new Google_Service_Sheets($client);
+        // Data to append to Google Sheets
+        $values = [
+            [
+                'Auto Sheets',
+                $data['name'],
+                $data['phone_number'],
+                $data['email'],
+                $data['messages'],
+                $submissionDate,
+                $submissionDate,
+            ]
+        ];
+
+        // Prepare the range where to append data in the Google Sheet
+        $range = 'Contact Form'; // Adjust this if necessary
+
+        $body = new Google_Service_Sheets_ValueRange([
+            'values' => $values
+        ]);
+
+        $params = [
+            'valueInputOption' => 'RAW',
+        ];
+
+        // Append the data to the spreadsheet
+        $service->spreadsheets_values->append($this->spreadsheetId, $range, $body, $params);
     }
 }
